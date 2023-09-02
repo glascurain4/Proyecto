743ce8da7f3646a8fdac623eb353a6d04aaed027
glascurain4 <142357491+glascurain4@users.noreply.github.com>
2023-09-01 19:33:36 -0600

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -3,8 +3,11 @@ pcom = float(input("Inserte el porcentaje de comisión (directa o compartida) en
 ppub = 0.0025
 
 com = precio*(pcom/100)
-pub = precio*ppub
+pub = precio*ppub    
 dif = com - pub
 rel = (pub/com)*100
 
-print("Los valores serían los siguientes: \n Valor propiedad:", precio, "\n Comisión:", com, "\n Pago por publicidad:", pub, "\n Comisión neta:", dif, "\n Porcentaje por publicidad respecto a la comisión:", rel,"%")
+def salida(precio, com, pub, rel):
+    print("Los valores serían los siguientes: \n Valor propiedad:", precio, "\n Comisión:", com, "\n Pago por publicidad:", pub, "\n Comisión neta:", dif, "\n Porcentaje por publicidad respecto a la comisión:", rel,"%")
+
+salida(precio, com, pub, rel)
