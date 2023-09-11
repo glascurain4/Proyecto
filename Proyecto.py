d905f512c90e8b460a0caea3ea749f6b3bb57aa1
glascurain4 <142357491+glascurain4@users.noreply.github.com>
2023-09-11 01:36:20 -0600

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -16,20 +16,23 @@ def evaluarpub(precio, pcom, ppub, com, pub, dif, rel):
     return print("Los resultados serían los siguientes \n Valor de la propiedad:", precio, "\n Comisión:",com,"\n Inversión en publicidad:", pub, "\n Comisón neta:", dif, "\n Porcentaje de publicidad respecto a la comisión:", rel,"%")
 
 #Logaritmo principal
-while True:
-    accion = str(input("¿Qué desea calcular? \n1. Comisión \n2. Comisión y Publicidad \n"))
+def main():
+    while True:
+        accion = str(input("¿Qué desea calcular? \n1. Comisión \n2. Comisión y Publicidad \n"))
 
-#Estructuras de decisión
-    if accion == "1":
-        precio = float(input("Precio de la propiedad a vender (en millones): \n"))*1000000
-        pcom = float(input("Inserte el porcentaje de comisión (directa o compartida) en %: \n"))
-        evaluarcom(precio, pcom, com)
-        break
-    elif accion == "2":
-        precio = float(input("Precio de la propiedad a vender (en millones): \n"))*1000000
-        pcom = float(input("Inserte el porcentaje de comisión (directa o compartida) en %: \n"))
-        ppub = 0.0025
-        evaluarpub(precio, pcom, ppub, com, pub, dif, rel)
-        break
-    else:
-        print("Respuesta invalida, intente de nuevo \n")
+    #Estructuras de decisión
+        if accion == "1":
+            precio = float(input("Precio de la propiedad a vender (en millones): \n"))*1000000
+            pcom = float(input("Inserte el porcentaje de comisión (directa o compartida) en %: \n"))
+            evaluarcom(precio, pcom, com)
+            break
+        elif accion == "2":
+            precio = float(input("Precio de la propiedad a vender (en millones): \n"))*1000000
+            pcom = float(input("Inserte el porcentaje de comisión (directa o compartida) en %: \n"))
+            ppub = 0.0025
+            evaluarpub(precio, pcom, ppub, com, pub, dif, rel)
+            break
+        else:
+            print("Respuesta invalida, intente de nuevo \n")
+
+main()
