b7938b7876594bde1974c7ff8dc765f3a8db411e
glascurain4 <142357491+glascurain4@users.noreply.github.com>
2023-10-16 17:16:35 -0600

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -1,12 +1,21 @@
-#Variables / Lista
+#Variables / Lista / Matrices
 datos = [0]*4
+resultados = [["Precio propiedad:",0],["Comisión:",0]]
+resultados2= [["Precio propiedad:",0], ["Comisión:",0], ["Valor publicidad:", 0], ["Comsión neta:",0], ["Relación publicidad/comisión:",0]]
 
 #Funciones
 def evaluarcom(datos):
-  return print("Los resultados serían los siguientes \n Valor de la propiedad:",datos[0], "\n Comisión:",(datos[0]*(datos[1]/100)))
-  
+  resultados[0][1]= datos[0]
+  resultados[1][1]= datos[0]*(datos[1]/100)
+  return print("Los resultados serían los siguientes \n", resultados[0], "\n", resultados[1])
+
 def evaluarpub(datos):
-  return print("Los resultados serían los siguientes \n Valor de la propiedad:", datos[0], "\n Comisión:",(datos[0]*(datos[1]/100)),"\n Inversión en publicidad:", datos[0]*datos[2], "\n Comisón neta:", (datos[0]*(datos[1]/100)-datos[0]*datos[2]), "\n Porcentaje de publicidad respecto a la comisión:", ((datos[0]*datos[2])/(datos[0]*(datos[1]/100))*100),"%")
+  resultados2[0][1]= datos[0]
+  resultados2[1][1]= datos[0]*(datos[1]/100)
+  resultados2[2][1]= datos[0]*datos[2]
+  resultados2[3][1]= (datos[0]*(datos[1]/100)-datos[0]*datos[2])
+  resultados2[4][1]= (datos[0]*datos[2])/(datos[0]*(datos[1]/100))*100
+  return print("Los resultados serían los siguientes: \n", resultados2[0], "\n", resultados2[1], "\n", resultados2[2], "\n ", resultados2[3], "\n ", resultados2[4], "%")
 
 #Logaritmo principal
 def main():
